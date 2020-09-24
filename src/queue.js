fe7c565a4b6abca9abe7346240a52b4d03bc9d5f
Jeanluca <jeanlucafp@gmail.com>
2020-09-24 11:33:45 +0000

    Update MultiRoteirizacaoJob.js

--- src/queue.js
+++ src/queue.js
@@ -1,2 +1,13 @@
 require('dotenv-extended').load()
+
+const Sentry = require("@sentry/node")
+
+if (process.env.SENTRY_DNS) {
+    console.log('iniciando monitoramento')
+    Sentry.init({
+        dsn: process.env.SENTRY_DNS,
+        tracesSampleRate: 1.0,
+    })
+}
+
 require('./queue/index').process()
